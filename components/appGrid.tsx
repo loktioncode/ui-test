9ff99f048bf4cea98e990b1bfcf938bc67552f00
Elisha <elishabere4@gmail.com>
2023-11-03 07:26:50 +0200

    add elisha

--- components/appGrid.tsx
+++ components/appGrid.tsx
@@ -4,6 +4,7 @@ import { Automation } from '@/lib/types';
 import React, { useContext } from 'react';
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from './ui/card';
 import Image from 'next/image'
+import { StaticImport } from 'next/dist/shared/lib/get-img-props';
 
 function AppGrid({ data }: {
   data: Automation[]
@@ -12,41 +13,29 @@ function AppGrid({ data }: {
 
 
   return (
-
-
-    <div className=" grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left gap-4">
-
-
-      {
-        filteredData.map((item: Automation, index: number) => (
-          <div key={index} className="group rounded-lg border border-transparent  transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
-            <Card className=''>
-              <CardHeader >
-                <Image
-                  src={item?.sites[0]?.logoSmall2x}
-                  alt="Vercel Logo"
-                  className="p-2"
-                  width={50}
-                  height={30}
-                  priority
-                />
-                <CardTitle className='text-lg font-bold text-black line-clamp-2'>
-                  {item?.title}
-                </CardTitle>
-
-              </CardHeader>
-              <CardContent>
-                <CardDescription className='text-sm line-clamp-3'>{item?.shortDescription
-                }</CardDescription>
-              </CardContent>
-
-            </Card>
-          </div>
-
-        ))
-      }
-
-
+ <div className="grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 md:grid-cols-2 sm:grid-cols-1 lg:text-left gap-4">
+      {filteredData.map((item: { sites: { logoSmall2x: string | StaticImport; }[]; title: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | React.PromiseLikeOfReactNode | null | undefined; shortDescription: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | React.PromiseLikeOfReactNode | null | undefined; }, index: React.Key | null | undefined) => (
+        <div key={index} className="group rounded-lg border border-transparent transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
+          <Card className="">
+            <CardHeader>
+              <Image
+                src={item?.sites[0]?.logoSmall2x}
+                alt="Vercel Logo"
+                className="p-2"
+                width={50}
+                height={30}
+                priority
+              />
+              <CardTitle className="text-lg font-bold text-black line-clamp-2">
+                {item?.title}
+              </CardTitle>
+            </CardHeader>
+            <CardContent>
+              <CardDescription className="text-sm line-clamp-3">{item?.shortDescription}</CardDescription>
+            </CardContent>
+          </Card>
+        </div>
+      ))}
     </div>
 
   );
