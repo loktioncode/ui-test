c89f0c0f8fe22ca836e8d336c6937eca85e79516
Elisha <elishabere4@gmail.com>
2023-11-03 09:48:13 +0200

    update app

--- app/page.tsx
+++ app/page.tsx
@@ -1,50 +1,59 @@
 'use client'
+
 import AppGrid from '@/components/appGrid'
 import Filters from '@/components/ui/filters'
 import { data } from '@/constants'
-import { FilteredDataContext } from '@/lib/data-context'
-import { Automation } from '@/lib/types'
+import { ArrowLeftCircle, ArrowRightCircle } from 'lucide-react'
 import Image from 'next/image'
-import { useContext, useEffect, useState } from 'react'
+import { useRef } from 'react'
 
 export default function Home() {
+  const outerContainerRef = useRef<HTMLDivElement>(null);
 
+  const handleScroll = (scrollAmount: number) => {
+    if (outerContainerRef.current) {
+      const newScrollLeft = outerContainerRef.current.scrollLeft + scrollAmount;
+      outerContainerRef.current.scrollLeft = newScrollLeft;
+    }
+  };
 
   return (
-    <main className="flex  flex-col items-center justify-between p-24">
-      <div className=" max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex ">
-        <a href="https://github.com/loktioncode/ui-test" target="_blank" rel="noopener noreferrer">
-          <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-            Clone Repo & run &nbsp;
+    <main className="flex flex-col items-center p-4 lg:p-24">
+      <div className="max-w-5xl w-full text-center lg:flex justify-between">
+        <a
+          href="https://github.com/loktioncode/ui-test"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="bg-gradient-to-b pb-6 pt-8 dark:from-inherit"
+        >
+          <p className="border-b border-gray-300 lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+            Clone Repo & run&nbsp;
             <code className="font-mono font-bold">yarn install</code>
           </p>
         </a>
 
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+        <div className="bg-gradient-to-t  lg:bg-none lg:static lg:w-auto lg:justify-end">
           <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
             href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
+            className="flex place-items-center gap-2 lg:p-0 text-xl font-semibold"
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
+            FE Developer Interview Challenge{' '}
+         
           </a>
         </div>
       </div>
-      <div className='my-10'>
+      <div className='my-10 flex flex-row items-center'>
+        <div className="lg:hidden cursor-pointer" onClick={() => handleScroll(200)}>
+          <ArrowLeftCircle />
+        </div>
         <Filters data={data?.data?.oneClickAutomations?.items} />
+        <div className="lg:hidden cursor-pointer" onClick={() => handleScroll(200)}>
+          <ArrowRightCircle className='text-red' />
+        </div>
       </div>
-
-      <AppGrid data={[...data?.data?.oneClickAutomations?.items]} />
-
+      <AppGrid data={data?.data?.oneClickAutomations?.items} />
     </main>
   )
 }
